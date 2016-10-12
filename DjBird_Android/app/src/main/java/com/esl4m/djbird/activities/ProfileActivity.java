3f3adcf54af4fbf16d10a7b808596821966a4782
eslam <eslam.diaa@espace.com.eg>
2016-10-12 10:37:57 +0200

    Adds header and profile picture to user profile

--- DjBird_Android/app/src/main/java/com/esl4m/djbird/activities/ProfileActivity.java
+++ DjBird_Android/app/src/main/java/com/esl4m/djbird/activities/ProfileActivity.java
@@ -4,9 +4,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.esl4m.djbird.R;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -24,6 +26,14 @@ public class ProfileActivity extends AppCompatActivity {
 
         TextView list_users = (TextView) findViewById(R.id.profile_text);
         list_users.setText("My Profile " + username + "  " + email + " " + profile_picture);
+        ImageView profile_pic = (ImageView) findViewById(R.id.profile_pic);
+//        Log.v("profile_picture","profile_picture : " + profile_picture);
+//        Log.v("profile_picture","profile_picture : " + R.string.api_base_url+"static/profile_pictures/"+profile_picture);
+        if (profile_picture.equals("default-pic.jpg")){
+            Picasso.with(this).load(R.drawable.defaultpic).into(profile_pic);
+        } else {
+            Picasso.with(this).load(getString(R.string.api_base_url)+"static/profile_pictures/"+profile_picture).into(profile_pic);
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
